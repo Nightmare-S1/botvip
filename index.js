24c7922e2dca23e68461fed13e8cd318fc7c3222
Replit user <>
2022-11-06 08:41:38 +0700

    1

--- index.js
+++ index.js
@@ -1,52 +1,53 @@
 const {
-    Client, 
-    MessageEmbed,
-    Collection
+  Client,
+  MessageEmbed,
+  Collection
 } = require('discord.js');
 const bot = new Client;
 
-const token = 'OTY2MzYyMTU5Mjg0MTI1Nzc2.G53zls.I-fd3hwsclJVlUF0ysPzTnqvKVsU6H9tVDsEAM';
+const token = 'OTY2MzYyMTU5Mjg0MTI1Nzc2.GFYVWn._8eGVc3fphh4x3vYLuveGEUcxQUqp6zRLdb9c0';
 var PREFIX = '>';
 var version = '1.1.2';
 
+require("http").createServer((_, res) => res.end("Uptime!")).listen(8080)
 
 const fs = require('fs');
 const commands = new Collection();
 const files = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-for(const file of files) {
-        const command = require(`./commands/${file}`);
-        commands.set(command.name, command)
+for (const file of files) {
+  const command = require(`./commands/${file}`);
+  commands.set(command.name, command)
 }
 
 
 bot.on('ready', () => {
-    console.log('Bot sudah on')
+  console.log('Bot sudah on')
 
-    bot.user.setActivity('Discord SOON Agar Ramai', {
-        type: "PLAYING"
-    }).catch(console.error);
+  bot.user.setActivity('Discord SOON Agar Ramai', {
+    type: "PLAYING"
+  }).catch(console.error);
 });
 
 bot.on('message', message => {
-    let args = message.content.substring(PREFIX.length).split(' ');
-    
-    switch (args[0]) {
-        case 'ping':
-            commands.get('ping').execute(message);
-            break;
+  let args = message.content.substring(PREFIX.length).split(' ');
 
-         case 'info':
-             commands.get('info').execute(message, args);
-             break;
-                    
-        case 'help':
-            commands.get('help').execute(message);
-            break;
+  switch (args[0]) {
+    case 'ping':
+      commands.get('ping').execute(message);
+      break;
 
-        case 'clear':
-            commands.get('clear').execute(message, args);
-            break;
-    }
+    case 'info':
+      commands.get('info').execute(message, args);
+      break;
+
+    case 'help':
+      commands.get('help').execute(message);
+      break;
+
+    case 'clear':
+      commands.get('clear').execute(message, args);
+      break;
+  }
 
 })
 
